3a9a5fdbcef71e40013ee7a640472e54b54dd500
Arunava Dey <adey26003@gmail.com>
2020-10-13 10:06:55 +0530

    first program
    Array operations

---
+++ assgn1.pl
@@ -0,0 +1,10 @@
+@arr=(1..10);
+print "Initial array: @arr\n";
+pop @arr;
+print "After pop: @arr\n";
+push @arr,(10);
+print "After push: @arr\n";
+shift @arr;
+print "After shift: @arr\n";
+unshift @arr,(1);
+print "After unshift: @arr\n";
